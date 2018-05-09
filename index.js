dc07b09531a3a4d4025a9c359c6aa025cb75d077
Yuriy Tymkiv <11hacha11@gmail.com>
2018-05-09 11:17:43 +0300

    cdfg

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const token = "552905592:AAFCbkNfdc2zn7ABBv7T1dKAg7RJuXOhC78";
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', msg=>{
-    bot.sendMessage(msg.chat.id, "Hello tranger");
+    bot.sendMessage(msg.chat.id, "Hello stranger");
 })
